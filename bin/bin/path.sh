1b1af9762cc7f08100479f52bd9b8a50c5b006d5
shum <sdothum@gmail.com>
2023-08-29 18:17:34 -0400

    accessibility conversion to hardtabs

--- bin/bin/path.sh
+++ bin/bin/path.sh
@@ -4,10 +4,9 @@
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ;then
-  PATH=$(echo $PATH | sed -e "s|$HOME/bin:||" -e "s|$HOME/.local/bin:||" -e 's|/opt/bin:||')
-  PATH="$HOME/.local/bin:/opt/bin:$PATH"
-  for i in $(find -L $HOME/bin -type d | grep -v '/\.' | sort -r)
-  do
-    PATH="${i}:$PATH"
-  done
+	PATH=$(echo $PATH | sed -e "s|$HOME/bin:||" -e "s|$HOME/.local/bin:||" -e 's|/opt/bin:||')
+	PATH="$HOME/.local/bin:/opt/bin:$PATH"
+	for i in $(find -L $HOME/bin -type d | grep -v '/\.' | sort -r) ;do
+		PATH="${i}:$PATH"
+	done
 fi
