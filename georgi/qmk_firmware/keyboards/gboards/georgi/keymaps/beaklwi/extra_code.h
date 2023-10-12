55bdc85e4ca0be14cf9393fc8fa408095b7a33d9
shum <sdothum@gmail.com>
2023-10-12 12:47:04 -0400

    tabs

--- georgi/qmk_firmware/keyboards/gboards/georgi/keymaps/beaklwi/extra_code.h
+++ georgi/qmk_firmware/keyboards/gboards/georgi/keymaps/beaklwi/extra_code.h
@@ -2,27 +2,27 @@
 // shorter version max refactoring (defaults removed: does not change code size)
 
 void cap(const struct Chord* self) {
-  switch (*self->state) {
-  case ACTIVATED:
-    tap_key(self->value1);
-    tap_key(self->value2);
-    break;
-  case DEACTIVATED:
-    current_pseudolayer = CAPS;
-    *self->state = IN_ONE_SHOT;
-    break;
-  case FINISHED:
-  case PRESS_FROM_ACTIVE:
-    current_pseudolayer = CAPS;
-    a_key_went_through = false;
-    break;
-  case RESTART:
-    if (a_key_went_through) {
-      current_pseudolayer = self->pseudolayer;
-    } else {
-      *self->state = IN_ONE_SHOT;
-    }
-  }
+	switch (*self->state) {
+	case ACTIVATED:
+		tap_key(self->value1);
+		tap_key(self->value2);
+		break;
+	case DEACTIVATED:
+		current_pseudolayer = CAPS;
+		*self->state = IN_ONE_SHOT;
+		break;
+	case FINISHED:
+	case PRESS_FROM_ACTIVE:
+		current_pseudolayer = CAPS;
+		a_key_went_through = false;
+		break;
+	case RESTART:
+		if (a_key_went_through) {
+			current_pseudolayer = self->pseudolayer;
+		} else {
+			*self->state = IN_ONE_SHOT;
+		}
+	}
 }
 
 static uint16_t postfix = KC_G;
@@ -31,40 +31,40 @@ static uint16_t pairs[][3] = { {KC_NO, KC_LBRC, KC_RBRC}, {KC_LSFT, KC_9, KC_0},
 static uint8_t bracket = 0;
 
 void output(int16_t modifier, int16_t keycode) {
-  key_in(modifier);
-  tap_key(keycode);
-  key_out(modifier);
+	key_in(modifier);
+	tap_key(keycode);
+	key_out(modifier);
 }
 
 void hexpad(const struct Chord* self) {
-  switch (*self->state) {
-  case ACTIVATED:
-    switch (self->value1) {
-    case 0:
-      switch (self->value2) {
-      case 1:
-        postfix = postfix == KC_G ? KC_SPC : KC_G;
-        break;
-      case 2:
-        bracket = (bracket == 0) ? 1 : ((bracket == 1) ? 2 : 0);
-        break;
-      case 0:
-        current_pseudolayer = BEAKL;
-        if (!postcap) {
-          break;
-        }
-      case 3:
-        tap_key(KC_CAPS);
-        postcap = !postcap;
-      }
-      break;
-    case 1:
-      output(postcap == 0 ? KC_LSFT : KC_NO, postfix);
-      break;
-    case 2:
-      output(pairs[bracket][0], pairs[bracket][self->value2]);
-    }
-  }
+	switch (*self->state) {
+	case ACTIVATED:
+		switch (self->value1) {
+		case 0:
+			switch (self->value2) {
+			case 1:
+				postfix = postfix == KC_G ? KC_SPC : KC_G;
+				break;
+			case 2:
+				bracket = (bracket == 0) ? 1 : ((bracket == 1) ? 2 : 0);
+				break;
+			case 0:
+				current_pseudolayer = BEAKL;
+				if (!postcap) {
+					break;
+				}
+			case 3:
+				tap_key(KC_CAPS);
+				postcap = !postcap;
+			}
+			break;
+		case 1:
+			output(postcap == 0 ? KC_LSFT : KC_NO, postfix);
+			break;
+		case 2:
+			output(pairs[bracket][0], pairs[bracket][self->value2]);
+		}
+	}
 }
 
 static uint8_t harmonic = 0;
@@ -72,17 +72,17 @@ static uint8_t harmonic = 0;
 #define interval(k) if (record->event.pressed) { harmonic |= k; if (harmonic == 3) { layer_move(0); return false; } } else { harmonic &= ~k; } break
 
 bool process_steno_user(uint16_t keycode, keyrecord_t *record) { 
-  switch (keycode) {
-  case STN_FN:
-    interval(1);
-  case STN_PWR:
-    interval(2);
-  }
-  return true;
+	switch (keycode) {
+	case STN_FN:
+		interval(1);
+	case STN_PWR:
+		interval(2);
+	}
+	return true;
 }
 
 void matrix_init_user() {
-  steno_set_mode(STENO_MODE_GEMINI);
+	steno_set_mode(STENO_MODE_GEMINI);
 }
 
 // json paste
