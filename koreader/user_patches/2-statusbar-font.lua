9720a1f6eabdcbde5920b0d3de77bace26044ff4
Steven Hum <sdothum@gmail.com>
2024-02-17 17:09:02 -0500

    merge alt-statusbar font patch

--- koreader/user_patches/2-statusbar-font.lua
+++ koreader/user_patches/2-statusbar-font.lua
@@ -1,18 +1,29 @@
 -- koreader/patches
 -- sdothum - 2016 (c) wtfpl
 --
+-- frontend/document/credocument.lua
 -- frontend/ui/font.lua
 --
--- change statusbar font to reading font
+-- set alt-/statusbar font to reading font
 
+-- my reading font
+local fontface = "drift"
+local fontsource = "drift-book.ttf"
+
+-- alt-statusbar header
+local CreDocument = require("document/credocument")
+
+CreDocument.header_font = fontface  -- see frontend/apps/reader/modules/readerfont.lua:onReadSettings()
+
+-- statusbar footer
 local Font = require("ui/font")
 
 for k, v in pairs(Font.fontmap) do
 	if k == "ffont" then
-		Font.fontmap[k] = "drift-book.ttf"
+		Font.fontmap[k] = fontsource
 	elseif k == "smallffont" then
-		Font.fontmap[k] = "drift-book.ttf"
+		Font.fontmap[k] = fontsource
 	elseif k == "largeffont" then
-		Font.fontmap[k] = "drift-book.ttf"
+		Font.fontmap[k] = fontsource
 	end
 end
