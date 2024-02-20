e05f98000a9b28b738f0508074af28a13604deac
Steven Hum <sdothum@gmail.com>
2024-02-19 20:29:16 -0500

    dynamic statusbar font update (to document default)

--- koreader/user_patches/2-statusbar-font.lua
+++ koreader/user_patches/2-statusbar-font.lua
@@ -1,31 +1,46 @@
 -- koreader/patches
 -- sdothum - 2016 (c) wtfpl
 --
+-- settings.reader.lua
 -- frontend/document/credocument.lua
 -- frontend/ui/font.lua
 --
--- set alt-/statusbar font to reading font
+-- match alt-/statusbar font to document font set as "default"
 
--- my reading font
-local fontface = "drift"
-local fontsource = "drift-book.ttf"
+-- ATTENTION: KOReader must be restarted to apply "default" font change and the document settings reinitialized with..
+-- top msenu -> document -> document settings -> reset document settings to default -> reset
 
--- alt-statusbar header
+-- default document font family
+local DataStorage = require("datastorage")
+local G_reader_settings = require("luasettings"):open(DataStorage:getDataDir() .. "/settings.reader.lua")
+
+local fontface = G_reader_settings:readSetting("cre_font")
+
+-- set alt-statusbar font family
 local CreDocument = require("document/credocument")
 
-# ATTENTION: if the expected alt-statusbar font does not display for the current document, tap..
-# top menu -> document -> document settings -> reset document settings to default
 CreDocument.header_font = fontface  -- see frontend/apps/reader/modules/readerfont.lua:onReadSettings()
 
--- statusbar footer
+-- set statusbar font source
 local Font = require("ui/font")
 
-for k, v in pairs(Font.fontmap) do
-	if k == "ffont" then
-		Font.fontmap[k] = fontsource
-	elseif k == "smallffont" then
-		Font.fontmap[k] = fontsource
-	elseif k == "largeffont" then
-		Font.fontmap[k] = fontsource
+names = { "-book.ttf", "-book.otf", "-regular.ttf", "-regular.otf" }  -- selection order of user ttf and otf font files
+
+for i, n in pairs(names) do
+	local f = io.open(DataStorage:getDataDir() .. "/fonts/" .. fontface .. n, "r")
+	if f ~= nil then  -- font source exists
+		io.close(f)
+		local fontsource = fontface .. n
+		for k, v in pairs(Font.fontmap) do
+			if k == "ffont" then
+				Font.fontmap[k] = fontsource
+			elseif k == "smallffont" then
+				Font.fontmap[k] = fontsource
+			elseif k == "largeffont" then
+				Font.fontmap[k] = fontsource
+			end
+		end
+		break
 	end
 end
+
