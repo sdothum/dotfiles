b44eb3137c81a61dc92d167ec84631369ef132d8
Steven Hum <sdothum@gmail.com>
2025-08-04 21:35:38 -0400

    supernote paging zones

--- supernote/koreader/defaults.custom.lua
+++ supernote/koreader/defaults.custom.lua
@@ -22,8 +22,8 @@ DTAP_ZONE_MENU_EXT     = { x = 0.25,  y = 0,     h = 0.12,  w = 0.5  },
 DTAP_ZONE_CONFIG       = { x = 0,     y = 0.94,  h = 0.06,  w = 1    },  -- bottom layout bar
 DTAP_ZONE_CONFIG_EXT   = { x = 0.25,  y = 0.91,  h = 0.09,  w = 0.5  },
 
-DTAP_ZONE_FORWARD      = { x = 0.03,  y = 0,     h = 1,     w = 0.97 },  -- page advance edge
-DTAP_ZONE_BACKWARD     = { x = 0,     y = 0,     h = 1,     w = 0.03 },
+DTAP_ZONE_FORWARD      = { x = 0.08,  y = 0,     h = 1,     w = 0.92 },  -- page advance edge
+DTAP_ZONE_BACKWARD     = { x = 0,     y = 0,     h = 1,     w = 0.08 },
 
 DTAP_ZONE_TOP_LEFT     = { x = 0,     y = 0,     h = 0.06,  w = 0.05 },  -- long press corners
 DTAP_ZONE_TOP_RIGHT    = { x = 0.95,  y = 0,     h = 0.06,  w = 0.05 },
