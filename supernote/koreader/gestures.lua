2f771602ea67db79978468aa3428e94d5c70c916
Steven Hum <sdothum@gmail.com>
2026-03-04 10:01:09 -0500

    update

---
+++ supernote/koreader/gestures.lua
@@ -0,0 +1,298 @@
+-- /storage/emulated/0/koreader/settings/gestures.lua
+return {
+    ["custom_multiswipes"] = {},
+    ["gesture_fm"] = {
+        ["hold_top_right_corner"] = {
+            ["refresh_content"] = true,
+        },
+        ["multiswipe"] = {},
+        ["multiswipe_east_north"] = {
+            ["history"] = true,
+        },
+        ["multiswipe_east_north_west"] = {},
+        ["multiswipe_east_north_west_east"] = {},
+        ["multiswipe_east_south"] = {
+            ["fm_go_to"] = true,
+        },
+        ["multiswipe_east_south_west_north"] = {
+            ["full_refresh"] = true,
+        },
+        ["multiswipe_east_west"] = {},
+        ["multiswipe_east_west_east"] = {
+            ["favorites"] = true,
+        },
+        ["multiswipe_north_east"] = {},
+        ["multiswipe_north_south"] = {
+            ["folder_up"] = true,
+        },
+        ["multiswipe_north_south_north"] = {},
+        ["multiswipe_north_west"] = {
+            ["folder_shortcuts"] = true,
+        },
+        ["multiswipe_northwest_southwest_northwest"] = {
+            ["toggle_wifi"] = true,
+        },
+        ["multiswipe_south_east"] = {},
+        ["multiswipe_south_east_north"] = {},
+        ["multiswipe_south_east_north_south"] = {},
+        ["multiswipe_south_north"] = {},
+        ["multiswipe_south_north_south"] = {},
+        ["multiswipe_south_west"] = {
+            ["show_frontlight_dialog"] = true,
+        },
+        ["multiswipe_southeast_northeast"] = {},
+        ["multiswipe_southeast_northeast_northwest"] = {
+            ["wifi_on"] = true,
+        },
+        ["multiswipe_southeast_southwest_northwest"] = {
+            ["wifi_off"] = true,
+        },
+        ["multiswipe_west_east"] = {},
+        ["multiswipe_west_east_west"] = {
+            ["open_previous_document"] = true,
+        },
+        ["multiswipe_west_north"] = {},
+        ["multiswipe_west_south"] = {
+            ["fm_back"] = true,
+        },
+        ["one_finger_swipe_left_edge_down"] = {
+            ["decrease_frontlight"] = 0,
+        },
+        ["one_finger_swipe_left_edge_up"] = {
+            ["increase_frontlight"] = 0,
+        },
+        ["one_finger_swipe_right_edge_down"] = {
+            ["decrease_frontlight_warmth"] = 0,
+        },
+        ["one_finger_swipe_right_edge_up"] = {
+            ["increase_frontlight_warmth"] = 0,
+        },
+        ["short_diagonal_swipe"] = {
+            ["full_refresh"] = true,
+        },
+        ["tap_left_bottom_corner"] = {
+            ["toggle_frontlight"] = true,
+        },
+        ["two_finger_swipe_north"] = {
+            ["increase_frontlight"] = 0,
+        },
+        ["two_finger_swipe_south"] = {
+            ["decrease_frontlight"] = 0,
+        },
+        ["two_finger_swipe_west"] = {
+            ["folder_shortcuts"] = true,
+        },
+    },
+    ["gesture_reader"] = {
+        ["double_tap_bottom_left_corner"] = {
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "toggle_wifi",
+                },
+            },
+            ["toggle_wifi"] = true,
+        },
+        ["double_tap_bottom_right_corner"] = {
+            ["profile_exec_gotham"] = true,
+            ["profile_exec_grotesque"] = true,
+            ["profile_exec_nagoya"] = true,
+            ["profile_exec_ottawa"] = true,
+            ["profile_exec_singspiel"] = true,
+            ["settings"] = {
+                ["always_active"] = true,
+                ["anchor_quickmenu"] = true,
+                ["order"] = {
+                    [1] = "profile_exec_gotham",
+                    [2] = "profile_exec_grotesque",
+                    [3] = "profile_exec_nagoya",
+                    [4] = "profile_exec_ottawa",
+                    [5] = "profile_exec_singspiel",
+                },
+                ["show_as_quickmenu"] = true,
+            },
+        },
+        ["double_tap_top_left_corner"] = {
+            ["page_browser"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "page_browser",
+                },
+            },
+        },
+        ["double_tap_top_right_corner"] = {
+            ["book_description"] = true,
+        },
+        ["hold_bottom_left_corner"] = {
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "show_vocab_builder",
+                },
+            },
+            ["show_vocab_builder"] = true,
+        },
+        ["hold_bottom_right_corner"] = {
+            ["history"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "history",
+                },
+            },
+        },
+        ["hold_top_left_corner"] = {
+            ["book_map"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "book_map",
+                },
+            },
+        },
+        ["hold_top_right_corner"] = {
+            ["bookmarks"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "bookmarks",
+                },
+            },
+        },
+        ["multiswipe"] = {},
+        ["multiswipe_east_north"] = {
+            ["history"] = true,
+        },
+        ["multiswipe_east_north_west"] = {
+            ["zoom"] = "contentwidth",
+        },
+        ["multiswipe_east_north_west_east"] = {
+            ["zoom"] = "pagewidth",
+        },
+        ["multiswipe_east_south"] = {
+            ["go_to"] = true,
+        },
+        ["multiswipe_east_south_west_north"] = {
+            ["full_refresh"] = true,
+        },
+        ["multiswipe_east_west"] = {
+            ["latest_bookmark"] = true,
+        },
+        ["multiswipe_east_west_east"] = {
+            ["favorites"] = true,
+        },
+        ["multiswipe_north_east"] = {
+            ["toc"] = true,
+        },
+        ["multiswipe_north_south"] = {},
+        ["multiswipe_north_south_north"] = {
+            ["prev_chapter"] = true,
+        },
+        ["multiswipe_north_west"] = {
+            ["bookmarks"] = true,
+        },
+        ["multiswipe_northwest_southwest_northwest"] = {
+            ["toggle_wifi"] = true,
+        },
+        ["multiswipe_south_east"] = {
+            ["toggle_reflow"] = true,
+        },
+        ["multiswipe_south_east_north"] = {
+            ["zoom"] = "contentheight",
+        },
+        ["multiswipe_south_east_north_south"] = {
+            ["zoom"] = "pageheight",
+        },
+        ["multiswipe_south_north"] = {
+            ["skim"] = true,
+        },
+        ["multiswipe_south_north_south"] = {
+            ["next_chapter"] = true,
+        },
+        ["multiswipe_south_west"] = {
+            ["show_frontlight_dialog"] = true,
+        },
+        ["multiswipe_southeast_northeast"] = {
+            ["follow_nearest_link"] = true,
+        },
+        ["multiswipe_southeast_northeast_northwest"] = {
+            ["wifi_on"] = true,
+        },
+        ["multiswipe_southeast_southwest_northwest"] = {
+            ["wifi_off"] = true,
+        },
+        ["multiswipe_west_east"] = {
+            ["previous_location"] = true,
+        },
+        ["multiswipe_west_east_west"] = {
+            ["open_previous_document"] = true,
+        },
+        ["multiswipe_west_north"] = {
+            ["book_status"] = true,
+        },
+        ["multiswipe_west_south"] = {
+            ["back"] = true,
+        },
+        ["one_finger_swipe_bottom_edge_left"] = {
+            ["previous_location"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "previous_location",
+                },
+            },
+        },
+        ["one_finger_swipe_bottom_edge_right"] = {
+            ["next_location"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "next_location",
+                },
+            },
+        },
+        ["one_finger_swipe_top_edge_left"] = {
+            ["calibre_search"] = true,
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "calibre_search",
+                },
+            },
+        },
+        ["one_finger_swipe_top_edge_right"] = {
+            ["settings"] = {
+                ["order"] = {
+                    [1] = "toc",
+                },
+            },
+            ["toc"] = true,
+        },
+        ["pinch_gesture"] = {
+            ["decrease_font"] = 0,
+        },
+        ["spread_gesture"] = {
+            ["increase_font"] = 0,
+        },
+        ["tap_left_bottom_corner"] = {
+            ["fulltext_search"] = true,
+        },
+        ["tap_right_bottom_corner"] = {
+            ["profile_exec_26x228%"] = true,
+            ["profile_exec_27x220%"] = true,
+            ["profile_exec_28x212%"] = true,
+            ["profile_exec_29x205%"] = true,
+            ["profile_exec_30x197%"] = true,
+            ["settings"] = {
+                ["always_active"] = true,
+                ["anchor_quickmenu"] = true,
+                ["order"] = {
+                    [1] = "profile_exec_26x228%",
+                    [2] = "profile_exec_27x220%",
+                    [3] = "profile_exec_28x212%",
+                    [4] = "profile_exec_29x205%",
+                    [5] = "profile_exec_30x197%",
+                },
+                ["show_as_quickmenu"] = true,
+            },
+        },
+        ["tap_top_left_corner"] = {
+            ["night_mode"] = true,
+        },
+        ["tap_top_right_corner"] = {
+            ["toggle_bookmark"] = true,
+        },
+    },
+}
