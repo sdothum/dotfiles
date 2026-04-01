3673b72bef3fcc55ca82a92645ebfb1464c4af4b
Steven Hum <sdothum@gmail.com>
2026-04-01 15:32:15 -0400

    chocofi rp2040

--- chocofi/qmk_firmware/keyboards/crkbd/keymaps/beaklwi/extra_code.h
+++ chocofi/qmk_firmware/keyboards/crkbd/keymaps/beaklwi/extra_code.h
@@ -22,4 +22,6 @@ void cap(const struct Chord* self) {
 	}
 }
 
+	"extra_code": "void cap(const struct Chord* self) {\n\tswitch (*self->state) {\n\tcase ACTIVATED:\n\t\ttap_key(self->value1);\n\t\ttap_key(self->value2);\n\t\tbreak;\n\tcase DEACTIVATED:\n\t\tcurrent_pseudolayer = CAPS;\n\t\t*self->state = IN_ONE_SHOT;\n\t\tbreak;\n\tcase FINISHED:\n\tcase PRESS_FROM_ACTIVE:\n\t\tcurrent_pseudolayer = CAPS;\n\t\ta_key_went_through = false;\n\t\tbreak;\n\tcase RESTART:\n\t\tif (a_key_went_through) {\n\t\t\tcurrent_pseudolayer = self->pseudolayer;\n\t\t} else {\n\t\t\t*self->state = IN_ONE_SHOT;\n\t\t}\n\t}\n}",
+
 	"extra_code": "void cap(const struct Chord* self) {\n switch (*self->state) {\n case ACTIVATED:\n tap_key(self->value1);\n tap_key(self->value2);\n break;\n case DEACTIVATED:\n current_pseudolayer = CAPS;\n *self->state = IN_ONE_SHOT;\n break;\n case FINISHED:\n case PRESS_FROM_ACTIVE:\n current_pseudolayer = CAPS;\n a_key_went_through = false;\n break;\n case RESTART:\n if (a_key_went_through) {\n current_pseudolayer = self->pseudolayer;\n } else {\n *self->state = IN_ONE_SHOT;\n }\n }\n }\n",
