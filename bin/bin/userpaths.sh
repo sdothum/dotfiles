7f49b2c5a80d7d63562fd430e240e626978c9d33
Steven Hum <sdothum@gmail.com>
2026-09-24 13:01:56 -0400

    .bashrc path; watchface; zephyr display dimension; xb check fix

--- bin/bin/userpaths.sh
+++ bin/bin/userpaths.sh
@@ -1,21 +1,38 @@
-# /etc/profile.d/path.sh
-# SEE: .xinitrc -> . /etc/profile
+#!/usr/bin/dash
+# sdothum - 2016 (c) wtfpl
+
+# Env
+# ══════════════════════════════════════════════════════════════════════════════
+
+# ............................................................. set session PATH
+
+# SEE: .bashrc
+
 [ $USER = root ] && exit
 
+# NOTE: update list as required for language environments
+
+SINGLES="
+	$HOME/.cargo/bin
+	$HOME/.nimble/bin
+	$HOME/go/bin
+"
 
 addpath() {
-	# echo $PATH | grep -q "$1:" && return
 	PATH="$1:$PATH"
 }
 
+# load /opt installations and other custom development environments
+# HINT: "ln -s ../<installation> bin" if bin is not part of native directoy structure
+#       or add appropriate directory to $SINGLES
+
 for i in $(find /opt -type d -name bin) ;do addpath "$i" ;done
-for i in $(find $HOME/go -type d -name bin) ;do addpath "$i" ;done
-for i in $(find $HOME/.cargo -type d -name bin | grep -Ev '/(git|registry)') ;do addpath "$i" ;done
-for i in $(find $HOME/.nimble -type d -name bin | grep -v '/pkgs.*') ;do addpath "$i" ;done
 for i in $(find $HOME/.Garmin -type d -name bin | grep -v 'AppImages') ;do addpath "$i" ;done
+for i in $SINGLES ;do addpath "$i" ;done
 
 # set PATH so it includes user's private bin if it exists
 
-[ -d $HOME/bin ] && for i in $(find -L $HOME/bin -type d | grep -Ev '\.(deprecated|save|old)') ;do addpath "$i" ;done
+[ -L $HOME/bin ] && L=-L
+[ -d $HOME/bin ] && for i in $(find $L $HOME/bin -type d | grep -Ev '\.deprecated') ;do addpath "$i" ;done
 
 export PATH
